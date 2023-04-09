52970302d1fdc253a5797b8a76e4f4c09db6dd5a
Minh Doan <minhadoan@gmail.com>
2023-04-09 15:18:04 -0500

    creating a server side folder in case users will be needed

---
+++ server/routes/main.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+// const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+
+// router.get('/profile', authController.getProfile)
+
+// router.post("/login", authController.postLogin);
+
+// router.post("/signup", authController.postSignup);
+
+// router.put('/edit/:id', authController.editProfile)
+
+module.exports = router;
