80515cedaf60d6d46bff6d3f9071b88a4b1442a5
Minh Doan <minhadoan@gmail.com>
2023-04-13 15:06:12 -0500

    Added some statistics

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -12,7 +12,7 @@ export default function () {
                     <Button
                         backgroundColor="blanchedalmond"
                         children='Start Here'
-                        color='burlywood'
+                        color='#ad8e60'
                         fontSize="1.8rem"
                         height='80px'
                         width='200px'
@@ -22,15 +22,18 @@ export default function () {
                 <section className='homePanel'>
                     <article>
                         <img src="/assets/concept-of-to-do-list.png" alt="" />
-                        <p>description for 1st panel</p>
+                        <p>The total U.S. student debt by the end of 2022 is $1.76 trillion and the average undergraduate student loan debt is around $37,787.</p>
+                        <a href="https://www.bankrate.com/loans/student-loans/student-loan-debt-statistics/#key-statistics">source</a>
                     </article>
                     <article>
                         <img src="/assets/concept-of-to-do-list.png" alt="" />
-                        <p>description for 1st panel</p>
+                        <p>The total mortgage debt in the U.S. as of Q1 of 2023 is $12.04 trillion and the average mortgage debt in 2022 is $236,443.</p>
+                        <a href="https://www.fool.com/the-ascent/research/average-household-debt/">source</a>
                     </article>
                     <article>
                         <img src="/assets/concept-of-to-do-list.png" alt="" />
-                        <p>description for 1st panel</p>
+                        <p>Total credit card debt in the U.S. by Q1 of 2023 is nearly $988 billion and for the average american is $5,733.</p>
+                        <a href="https://www.cnbc.com/2023/06/09/how-much-credit-card-debt-americans-hold-by-age.html"></a>
                     </article>
                 </section>
             </section>
