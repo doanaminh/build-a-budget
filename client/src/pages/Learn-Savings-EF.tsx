21a8436249d0a00ffc3aae417ea95b5b1102611c
Minh Doan <minhadoan@gmail.com>
2023-05-22 16:37:24 -0500

    completed content for step 0 of prioritizing budget expenses

---
+++ client/src/pages/Learn-Savings-EF.tsx
@@ -0,0 +1,22 @@
+export default function () {
+    return (
+        <>
+            <section>
+                <h3></h3>
+                <p></p>
+                <article>
+                    <ol>
+                    <li>
+                            <h4>Build a Small Emergency Fund</h4>
+                            <p>A good starting point should be the bigger amount between $1,000 or one month's worth of expenses. (Refer to your budget for this amount). Using a high-yield savings account can earn you a tiny amount of extra money over time. The alternative is a checking account with a trusted bank.</p>
+                        </li>
+                        <li>
+                            <h4>Pay Any Non-Essential Bills in Full</h4>
+                            <p>A good starting point should be the bigger amount between $1,000 or one month's worth of expenses. (Refer to your budget for this amount). Using a high-yield savings account can earn you a tiny amount of extra money over time. The alternative is a checking account with a trusted bank.</p>
+                        </li>
+                    </ol>
+                </article>
+            </section>
+        </>
+    )
+}
