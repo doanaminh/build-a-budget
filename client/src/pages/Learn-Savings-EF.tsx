d276b4d6bdca64823880d83e776f6a90380cda92
Minh Doan <minhadoan@gmail.com>
2023-05-25 16:53:29 -0500

    adding content to savings-ef page

--- client/src/pages/Learn-Savings-EF.tsx
+++ client/src/pages/Learn-Savings-EF.tsx
@@ -2,13 +2,13 @@ export default function () {
     return (
         <>
             <section>
-                <h3></h3>
-                <p></p>
+                <h3>Build an Emergency Fund</h3>
+                <p>Good job on moving your money around your budget! At this point you should have extra money monthly and are looking to distribute it into a savings account of some sort.</p>
                 <article>
                     <ol>
-                    <li>
+                        <li>
                             <h4>Build a Small Emergency Fund</h4>
-                            <p>A good starting point should be the bigger amount between $1,000 or one month's worth of expenses. (Refer to your budget for this amount). Using a high-yield savings account can earn you a tiny amount of extra money over time. The alternative is a checking account with a trusted bank.</p>
+                            <p>A good starting point should be the bigger amount between $1,000 or one month's worth of expenses. (Refer to your budget for this amount; this should be the total amount in your needs category plus the total of your wants category). Using a high-yield savings account can earn you a tiny amount of extra money over time. The alternative is a checking account with a trusted bank. This is something you may have to slowly build up. The goal is to have one month of expenses saved up in case of an emergency. You could suddenly lose your way of generating income and this will give you one month to find another source of income.</p>
                         </li>
                         <li>
                             <h4>Pay Any Non-Essential Bills in Full</h4>
