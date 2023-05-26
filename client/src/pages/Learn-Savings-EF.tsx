5c1c354b96eac517638002641a5e95483c0e2a9e
Minh Doan <minhadoan@gmail.com>
2023-05-26 17:03:00 -0500

    finished content on efund page

--- client/src/pages/Learn-Savings-EF.tsx
+++ client/src/pages/Learn-Savings-EF.tsx
@@ -8,14 +8,15 @@ export default function () {
                     <ol>
                         <li>
                             <h4>Build a Small Emergency Fund</h4>
-                            <p>A good starting point should be the bigger amount between $1,000 or one month's worth of expenses. (Refer to your budget for this amount; this should be the total amount in your needs category plus the total of your wants category). Using a high-yield savings account can earn you a tiny amount of extra money over time. The alternative is a checking account with a trusted bank. This is something you may have to slowly build up. The goal is to have one month of expenses saved up in case of an emergency. You could suddenly lose your way of generating income and this will give you one month to find another source of income.</p>
+                            <p>A good starting point should be the bigger amount between $1,000 or 1 month's worth of expenses. (Refer to your budget for this amount; this should be the total amount in your needs category plus the total of your wants category). Using a high-yield savings account can earn you a tiny amount of extra money over time. The alternative is a checking account with a trusted bank. This is something you may have to slowly build up. The goal is to have 1 month of expenses saved up in case of an emergency. You could suddenly lose your way of generating income and this will give you 1 month to find another source of income.</p>
                         </li>
                         <li>
                             <h4>Pay Any Non-Essential Bills in Full</h4>
-                            <p>A good starting point should be the bigger amount between $1,000 or one month's worth of expenses. (Refer to your budget for this amount). Using a high-yield savings account can earn you a tiny amount of extra money over time. The alternative is a checking account with a trusted bank.</p>
+                            <p>This is AFTER you have built up your 1-month emergency fund. This includes: cable, internet, phone, etc. Pay it all off at your own pace.</p>
                         </li>
                     </ol>
                 </article>
+                <p>If you have completed the steps above, most people will now consider you financially stable. You may still have debt, but you are able to pay it off in a timely manner along with all of your other bills. Hopefully you now understand where your money is coming in and where it's going out. The next step will be to plan for retirement.</p>
             </section>
         </>
     )
