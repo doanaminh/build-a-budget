f88d16c38ac44a1c8257b02cab317354f03f7cd6
Minh Doan <minhadoan@gmail.com>
2023-06-02 14:32:41 -0500

    created debt page of learn topic

---
+++ client/src/pages/Learn-Debt.tsx
@@ -0,0 +1,19 @@
+export default function () {
+    return (
+        <>
+            <section>
+                <h3>Paying Off Debts</h3>
+                <p>This section deals with the difficult matters of handling and eliminating debt. Although at times it may seem impossible, there is a way to eliminate it altogether. It takes time, but with a solid plan, you can maximize the efficiency at which you can pay them off.</p>
+                <article>
+                    <ol>
+                        <li>
+                            <h4>Enroll in Employer-Matched Retirement Account</h4>
+                            <p>After you've asked your employer about what kind of retirement plans they offer, it's time to choose one and start contributing a part of your income towards it. The most common one is probably the 401(k) and it comes in either a Traditional or a Roth version. In plain terms, a Traditional 401(k) can be contributed to with money that HAS NOT been taxed, but will be taxed when you withdraw the profits in the future. This effectively does two things simultaneously: lowers your overall income which will potentially lower how much you will be taxed and allows you to potentially invest more per paycheck. This is a great option if you think you will have a lower income in the future. A Roth 401(k) could be considered the opposite. You contribute money that is already taxed, but you will not have to pay taxes on the profits when you withdraw it at a later date. This a good option if you plan to make MORE income in the future.</p>
+                        </li>
+                    </ol>
+                </article>
+                <p>Good job on taking your first step into investing in yourself and creating a future where you potentially don't have to work every single day just to survive. This section was the first step into a very complicated topic. It's okay to not remember everything. There are free resources in every corner of the internet that you can check when you have doubts about anything. Take a deep breath and push forward when you're ready.</p>
+            </section>
+        </>
+    )
+}
