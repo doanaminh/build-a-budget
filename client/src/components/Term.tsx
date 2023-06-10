373979cf8bd7d7ce235ddd8316435c708878354c
Minh Doan <minhadoan@gmail.com>
2023-06-10 16:23:54 -0500

    created a term component

---
+++ client/src/components/Term.tsx
@@ -0,0 +1,32 @@
+
+
+interface Props {
+    setOpenModal: any;
+    children?:React.ReactNode;
+    term: string;
+    definition: string;
+}
+
+const Modal: React.FC<Props> = ({
+    setOpenModal,
+    term,
+    definition,
+}) => {
+
+    return (
+        <>
+            <section className="budgetBody">
+                <section className="budgetBreakdown">
+                    <section className="budgetRemainder">
+                        <h4>{term}</h4>
+                    </section>
+                    <section className="budgetSections">
+                        <p>{definition}</p>
+                    </section>
+                </section>
+            </section>
+        </>
+    )
+}
+
+export default Modal;
