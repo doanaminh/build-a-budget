a5c15c26c6d2973451f2b4266dad61dfdebfc665
Minh Doan <minhadoan@gmail.com>
2023-06-23 14:36:41 -0500

    css for sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -11,44 +11,40 @@ const Sidebar: React.FC<Props> = () => {
 
     return (
         <>
-            <section
-                className="Sidebar"
-                onMouseEnter={() => setModalOpen(true)}
-                onMouseLeave={() => setModalOpen(false)}
-            >
-                {modalOpen && 
-                    <section>
+            <section className="Sidebar">
+                
+                <section className="tableContent">
+                    <ol>
                         <h4>Table of Contents</h4>
-                        <ol>
-                            <li>
-                                <a href="/learn-budget">Budget & reduce monthly expenses; set realistic goals</a>
-                            </li>
-                            <li>
-                                <a href="/learn-savings-ef">Build an emergency fund</a>
-                            </li>
-                            <li>
-                                <a href="/learn-employer-intro">Employer-sponsored matching funds</a>
-                            </li>
-                            <li>
-                                <a href="">Focus on paying high/moderate debts off</a>
-                            </li>
-                            <li>
-                                <a href="">Save for retirement in an IRA & other higher education expenses</a>
-                            </li>
-                            <li>
-                                <a href="">Max out retirement savings where possible</a>
-                            </li>
-                            <li>
-                                <a href="">Save for other goals & advanced methods</a>
-                            </li>
-                        </ol>
-                    </section>
-                }
+                        <li>
+                            <a href="/learn-budget">Budget & reduce monthly expenses; set realistic goals</a>
+                        </li>
+                        <li>
+                            <a href="/learn-savings-ef">Build an emergency fund</a>
+                        </li>
+                        <li>
+                            <a href="/learn-employer-intro">Employer-sponsored matching funds</a>
+                        </li>
+                        <li>
+                            <a href="">Focus on paying high/moderate debts off</a>
+                        </li>
+                        <li>
+                            <a href="">Save for retirement in an IRA & other higher education expenses</a>
+                        </li>
+                        <li>
+                            <a href="">Max out retirement savings where possible</a>
+                        </li>
+                        <li>
+                            <a href="">Save for other goals & advanced methods</a>
+                        </li>
+                    </ol>
+                </section>
 
+                <div></div>
 
-                <article>
+                <section className="sidebarIcon">
                     <span>{'>'}</span>
-                </article>
+                </section>
             </section>
 
 
--- client/src/pages/Learn-Budget.tsx
+++ client/src/pages/Learn-Budget.tsx
@@ -8,66 +8,69 @@ export default function () {
 
     return (
         <>
-            <section className="sidebar">
-                <Sidebar />
-            </section>
             <section className="Learn Page Budgeting">
-                <section>
-                    <h3>Build a budget!</h3>
-                    <p>The most important and basic thing you can do is create a monthly budget. Knowing where your money is coming in and going out is fundamental to being financially stable. No matter how stressful it is to look at these numbers, it is always better to know about the problem rather than ignoring it. That is the first step of devising a plan to fix the problem.</p>
-                    <Button 
-                        backgroundColor="black"
-                        color="white"
-                        fontSize="1rem"
-                        height="4rem"
-                        width="10rem"
-                        onClick={() => navigate("/budget-calculator")}
-                    >Create a budget here</Button>
-                    <section>
-                        <p>Good job! So now that you have your budget, let's move on to the next steps. The most immediate thing you can do next, is start moving around the numbers in your budget. The priority of where your money is going every month should be as follows (from most important first):</p>
-                        <ol>
-                            <li>
-                                <h4>Rent/Mortgage</h4>
-                                <p>This includes any renter's or homeowner's insurance if required. It is essential to have good health and sleep if you are to succeed.</p>
-                            </li>
-                            <li>
-                                <h4>Food/Groceries</h4>
-                                <p>Every single human being on this earth needs to eat. Eating gives you energy and will impact your health greatly. **In extreme cases, bills and utilities may need to be prioritized over food.</p>
-                            </li>
-                            <li>
-                                <h4>Essential Items</h4>
-                                <p>This includes: power, water, heat, toiletries/hygiene, etc. These are basic human needs that you cannot live without.</p>
-                            </li>
-                            <li>
-                                <h4>Income Earning Expenses</h4>
-                                <p>Necessary transportation expenses (gas, public transit fare), possibly internet/phone. Anything that is required for you to continue earning income.</p>
-                            </li>
-                            <li>
-                                <h4>Health Care</h4>
-                                <p>Health insurance and any healthcare expenses (NOT INCLUDING HEALTH CARE DEBT)Your health, both physically and mentally, is so important for you to succeed. I cannot stress this enough. Breaking down these expenses may start to seem inhumane and stressful but remember that you should do what you can and at your own pace. It's not about how fast you can finish the race, but finishing it period.</p>
-                            </li>
-                            <li>
-                                <h4>Minimum Payments on All Debts & Loans</h4>
-                                <p>This includes: student loans, credit cards, etc. The only time you should strive to pay off a debt faster is: when you can decrease the interest rate through refinancing. This is a complicated topic with complicated calculations and you should ask your lendor about options about this topic. Most of the time you want to pay the absolute minimum so you can put the rest of your income towards something else if needed.</p>
-                            </li>
-                        </ol>
-                    </section>
-                    <article>
-                        <p>If you have completed all the steps listed above, you are officially ready to start planning your savings! Awesome job! Let's keep moving.</p>
-                    </article>
-                    <article style={{display: 'flex', justifyContent: 'end'}}>
-                        <Button 
-                            backgroundColor="#535337"
-                            color="beige"
-                            fontSize="1rem"
-                            height="3rem"
-                            width="8rem"
-                            onClick={() => navigate('/learn-savings-ef')}
-                        >Next</Button>
-                    </article>
+                <section className="sidebar">
+                    <Sidebar />
                 </section>
-                
+                <section>
+                    <section>
+                        <h3>Build a budget!</h3>
+                        <p>The most important and basic thing you can do is create a monthly budget. Knowing where your money is coming in and going out is fundamental to being financially stable. No matter how stressful it is to look at these numbers, it is always better to know about the problem rather than ignoring it. That is the first step of devising a plan to fix the problem.</p>
+                        <Button 
+                            backgroundColor="black"
+                            color="white"
+                            fontSize="1rem"
+                            height="4rem"
+                            width="10rem"
+                            onClick={() => navigate("/budget-calculator")}
+                        >Create a budget here</Button>
+                        <section>
+                            <p>Good job! So now that you have your budget, let's move on to the next steps. The most immediate thing you can do next, is start moving around the numbers in your budget. The priority of where your money is going every month should be as follows (from most important first):</p>
+                            <ol>
+                                <li>
+                                    <h4>Rent/Mortgage</h4>
+                                    <p>This includes any renter's or homeowner's insurance if required. It is essential to have good health and sleep if you are to succeed.</p>
+                                </li>
+                                <li>
+                                    <h4>Food/Groceries</h4>
+                                    <p>Every single human being on this earth needs to eat. Eating gives you energy and will impact your health greatly. **In extreme cases, bills and utilities may need to be prioritized over food.</p>
+                                </li>
+                                <li>
+                                    <h4>Essential Items</h4>
+                                    <p>This includes: power, water, heat, toiletries/hygiene, etc. These are basic human needs that you cannot live without.</p>
+                                </li>
+                                <li>
+                                    <h4>Income Earning Expenses</h4>
+                                    <p>Necessary transportation expenses (gas, public transit fare), possibly internet/phone. Anything that is required for you to continue earning income.</p>
+                                </li>
+                                <li>
+                                    <h4>Health Care</h4>
+                                    <p>Health insurance and any healthcare expenses (NOT INCLUDING HEALTH CARE DEBT)Your health, both physically and mentally, is so important for you to succeed. I cannot stress this enough. Breaking down these expenses may start to seem inhumane and stressful but remember that you should do what you can and at your own pace. It's not about how fast you can finish the race, but finishing it period.</p>
+                                </li>
+                                <li>
+                                    <h4>Minimum Payments on All Debts & Loans</h4>
+                                    <p>This includes: student loans, credit cards, etc. The only time you should strive to pay off a debt faster is: when you can decrease the interest rate through refinancing. This is a complicated topic with complicated calculations and you should ask your lendor about options about this topic. Most of the time you want to pay the absolute minimum so you can put the rest of your income towards something else if needed.</p>
+                                </li>
+                            </ol>
+                        </section>
+                        <article>
+                            <p>If you have completed all the steps listed above, you are officially ready to start planning your savings! Awesome job! Let's keep moving.</p>
+                        </article>
+                        <article style={{display: 'flex', justifyContent: 'end'}}>
+                            <Button 
+                                backgroundColor="#535337"
+                                color="beige"
+                                fontSize="1rem"
+                                height="3rem"
+                                width="8rem"
+                                onClick={() => navigate('/learn-savings-ef')}
+                            >Next</Button>
+                        </article>
+                    </section>
+
+                </section>
             </section>
+
         </>
     )
 }
