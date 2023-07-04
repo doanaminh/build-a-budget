0263f8475f04ec5e97a27592b5a1fc52007d3f9b
Minh Doan <minhadoan@gmail.com>
2023-07-04 15:58:04 -0500

    changed footer and credits

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,10 +4,14 @@ export default function () {
     return (
         <>
             <div className='Footer'>
-                <ul>
-                    <li><a href="https://i.imgur.com/u0ocDRI.png" target='_blank'>REDDIT REFERENCE</a></li>
-                    <li><a href="/credits">credits</a></li>
-                </ul>
+                <article>
+                    <p>
+                        &copy;<a href="https://www.twitter.com/doanaminh">Minh Doan</a> |{' '}
+                        <span>
+                        <a href="/credits">Credits</a>
+                    </span>
+                    </p>
+                </article>
             </div>
         </>
     )
