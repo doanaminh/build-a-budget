9bde170bb692bd9dc977c457833c5ca9aa68ab12
Minh Doan <minhadoan@gmail.com>
2023-07-26 22:22:22 -0500

    removed some nav links

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,8 +6,6 @@ export default function () {
             <div className='Navbar'>
                 <h2><a href="/">BaB</a></h2>
                 <ul>
-                    <li><a href="/calculator">Start Here</a></li>
-                    <li><a href="">FAQ</a></li>
                     <li><a href="/calculator">Calculator</a></li>
                     <li><a href="/learn">Learn</a></li>
                     <li><a href="/about">About</a></li>
