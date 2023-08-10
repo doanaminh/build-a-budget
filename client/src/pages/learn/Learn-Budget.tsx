7bf568b13c208ece2bd8a17260efad7c3e37d222
Minh Doan <minhadoan@gmail.com>
2023-08-10 17:44:50 -0500

    more informationg edits on budget learn page

--- client/src/pages/learn/Learn-Budget.tsx
+++ client/src/pages/learn/Learn-Budget.tsx
@@ -36,7 +36,7 @@ export default function () {
                         </article>
                         
                         <article>
-                            <p>Great! So now that you have your budget, let's move on to the next steps. The most immediate thing you can do next, is start moving around the numbers in your budget. The priority of where your money should be going every month should be as follows (from most immediate first):</p>
+                            <p>Great! So now that you have your budget, let's prioritize where your money should be going (from most immediate first):</p>
                             <ol>
                                 <li>
                                     <h4>Rent/Mortgage</h4>
@@ -44,7 +44,7 @@ export default function () {
                                 </li>
                                 <li>
                                     <h4>Food/Groceries</h4>
-                                    <p>Every single human being on this earth needs to eat. Eating gives you energy and will impact your health greatly. **In extreme cases, bills and utilities may need to be prioritized over food.</p>
+                                    <p>Every single human being on this earth needs to eat. Eating gives you energy and will impact your health greatly. **In extreme cases, bills and utilities may need to be prioritized over food. Sadly, snacks are not included in this category.</p>
                                 </li>
                                 <li>
                                     <h4>Essential Items</h4>
@@ -56,16 +56,16 @@ export default function () {
                                 </li>
                                 <li>
                                     <h4>Health Care</h4>
-                                    <p>Health insurance and any healthcare expenses (NOT INCLUDING HEALTH CARE DEBT)Your health, both physically and mentally, is so important for you to succeed. I cannot stress this enough. Breaking down these expenses may start to seem inhumane and stressful but remember that you should do what you can and at your own pace. It's not about how fast you can finish the race, but finishing it period.</p>
+                                    <p>Health insurance and any healthcare expenses (NOT INCLUDING HEALTH CARE DEBT). Your health, both physically and mentally, is so important for you to succeed.</p>
                                 </li>
                                 <li>
                                     <h4>Minimum Payments on All Debts & Loans</h4>
-                                    <p>This includes: student loans, credit cards, etc. The only time you should strive to pay off a debt faster is: when you can decrease the interest rate through refinancing. This is a complicated topic with complicated calculations and you should ask your lendor about options about this topic. Most of the time you want to pay the absolute minimum so you can put the rest of your income towards something else if needed.</p>
+                                    <p>This includes: student loans, credit cards, etc. The only time you should strive to pay off a debt faster is: when you can decrease the interest rate through refinancing. This is a complicated topic with complicated calculations and you should ask your lendor about options about this topic. Most of the time you want to pay the absolute minimum so you can put the remainder to necessities or generate more income.</p>
                                 </li>
                             </ol>
                         </article>
                         <article>
-                            <p>If you have completed all the steps listed above, you are officially ready to start planning your savings! Awesome job! Let's keep moving.</p>
+                            <p>If you have completed all the steps listed above, you are officially ready to start planning your savings! Let's keep moving.</p>
                         </article>
                         <article className="buttons">
                             <Button 
