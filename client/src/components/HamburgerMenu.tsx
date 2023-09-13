5e08218f27f1c2fac7817cad77de08f04a071fc5
Minh Doan <minhadoan@gmail.com>
2023-09-13 12:05:55 -0500

    mobile view css for navbar

---
+++ client/src/components/HamburgerMenu.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function () {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <div className="hamburgerContainer">
+        <div className="hamburgerIcon" onClick={handleOpen}>
+          <span>&#8213;</span>
+          <span>&#8213;</span>
+          <span>&#8213;</span>
+        </div>
+      </div>
+      {open && (
+        <div className="navOpen">
+          <ul className="navOpenLinks">
+            <li>
+              <a href="/calculator">Calculator</a>
+            </li>
+            <li>
+              <a href="/learn">Learn</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/glossary">Glossary</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,33 @@
 import "./Navbar.css";
+import HamburgerMenu from "./HamburgerMenu";
 
 export default function () {
-    return (
-        <>
-            <div className='Navbar'>
-                <h2><a href="/" className="asdf"><span>BaB</span></a></h2>
-                <ul>
-                    <li><a href="/calculator">Calculator</a></li>
-                    <li><a href="/learn">Learn</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/glossary">Glossary</a></li>
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="Navbar">
+        <div className="navItems">
+          <h2 className="item-a">
+            <a href="/" className="asdf">
+              <span>BaB</span>
+            </a>
+          </h2>
+          <ul className="navLinks">
+            <li>
+              <a href="/calculator">Calculator</a>
+            </li>
+            <li>
+              <a href="/learn">Learn</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/glossary">Glossary</a>
+            </li>
+          </ul>
+        </div>
+        <HamburgerMenu />
+      </div>
+    </>
+  );
 }
