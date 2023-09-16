b422e2ba9af56cfaa4b4b0896e6c93f99b101bb5
Minh Doan <minhadoan@gmail.com>
2023-09-16 05:51:49 -0500

    updated footer link to portfolio

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,18 +1,18 @@
 import "./Footer.css";
 
 export default function () {
-    return (
-        <>
-            <div className='Footer'>
-                <article>
-                    <p>
-                        &copy;<a href="https://www.twitter.com/doanaminh">Minh Doan</a> |{' '}
-                        <span>
-                        <a href="/credits">Credits</a>
-                    </span>
-                    </p>
-                </article>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="Footer">
+        <article>
+          <p>
+            &copy;<a href="https://doanaminh.netlify.app">Minh Doan</a> |{" "}
+            <span>
+              <a href="/credits">Credits</a>
+            </span>
+          </p>
+        </article>
+      </div>
+    </>
+  );
 }
